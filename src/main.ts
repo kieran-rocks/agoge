0bf2aa3ca99cd5a63c4926facebf8af0658db92e
Peter <rphumulock@gmail.com>
2021-01-27 19:52:10 -0500

    Added PrimeVue and prettier config

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,14 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import PrimeVue from "primevue/config";
+
+import "primevue/resources/themes/saga-blue/theme.css"; //theme
+import "primevue/resources/primevue.min.css"; //core css
+import "primeicons/primeicons.css"; //icons
 
 createApp(App)
   .use(store)
   .use(router)
+  .use(PrimeVue)
   .mount("#app");
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="about">
-	<h1>Mat</h1>
-	<img alt="Vue logo" src="../assets/mat.jpg" />
+    <h1>Mat</h1>
+    <img alt="Vue logo" src="../assets/mat.jpg" />
   </div>
 </template>
